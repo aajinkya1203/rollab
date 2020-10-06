5949173f3ede495ecf0113f7e5356f075dd73249
Aajinkya Singh <43881544+aajinkya1203@users.noreply.github.com>
2020-10-06 10:05:21 +0530

    Added lyrics

---
+++ lyrics.js
@@ -0,0 +1,209 @@
+module.exports=[
+    {song: "roxanne" , lyric: "Never gonna love me but it's alright. She think I'm an a**hole" },
+    {song: "roxanne" , lyric: "Met her at a party in the Hills, yeah She just wanna do it for the thrill, yeah" },
+    {song: "roxanne" , lyric: "But if I throw this money she gon' drop, ayy She don't wait in lines if it's too long" },
+    {song: "roxanne" , lyric: "Only want to call when the cash out Only take the pic when her a** out" },
+
+    {song: "rich nigga shit" , lyric: "I be fresh as hell, rockin' Prada Hoppin' out the PJ with a model" },
+    {song: "rich nigga shit" , lyric: "This supercharger whine like the car mat Savage got a whole lotta hits, dawg" },
+    {song: "rich nigga shit" , lyric: "I be fresh as hell, rockin' Prada Hoppin' out the PJ with a model" },
+
+    {song: "laugh now cry later" , lyric: "Please don't play that niggas songs in this party, I can't even listen to that" },
+    {song: "laugh now cry later" , lyric: "When he tell the story, that's not how it went Know they be lying, a hundred percent" },
+    {song: "laugh now cry later" , lyric: "Distance between us is not like a store, this isn't a closeable gap, ayy" },
+    {song: "laugh now cry later" , lyric: "And then shawty came back and said she didn't mean it It's hard to believe it" },
+
+
+    {song: "WAP" , lyric: "I said, certified freak Seven days a week" },
+    {song: "WAP" , lyric: "Let's role play, I wear a disguise I want you to park that big Mack truck" },
+    {song: "WAP" , lyric: "Out in public, make a scene I don't cook, I don't clean" },
+
+    {song: "Cardigan" , lyric: "Dancin' in your Levi's Drunk under a streetlight" },
+    {song: "Cardigan" , lyric: "A friend to all is a friend to none Chase two girls, lose the one" },
+    
+    {song: "Rockstar" , lyric: "I've been fuckin' hoes and poppin' pillies Man, I feel just like a rockstar" },
+    {song: "Rockstar" , lyric: "Switch my whip, came back in black I'm startin' sayin', \"Rest in peace to Bon Scott\"" },
+    {song: "Rockstar" , lyric: "Shit was legendary Threw a TV out the window of the Montage" },
+
+    {song: "Beautiful Escape" , lyric: "I feel you breathing down my neck As the blood's rushing through my legs" },
+    {song: "Beautiful Escape" , lyric: "All this lovin' that I hold inside But now I feel your power" },
+    {song: "Beautiful Escape" , lyric: "Oh I'm trying to let you know I just wanna go there" },
+
+    {song: "The Scotts" , lyric: "You lettin' the Scotts outside (yeah) We runnin' the scouts" },
+    {song: "The Scotts" , lyric: "She drink a lot of the bourbon (yeah) Like she from the street, uh" },
+    {song: "The Scotts" , lyric: "Straight to the couch We put the mic outside" },
+
+    {song: "Blinding Lights" , lyric: "I said, ooh, I'm drowning in the night Oh, when I'm like this, you're the one I trust" },
+    {song: "Blinding Lights" , lyric: "I'm running out of time 'Cause I can see the sun light up the sky" },
+    {song: "Blinding Lights" , lyric: "I been tryna call I been on my own for long enough" },
+
+    {song: "The Box" , lyric: "She sucked a nigga soul, gotta Cash App Told 'em wipe a nigga nose, say slatt" },
+    {song: "The Box" , lyric: "Niggas tryna get me for my water" },
+    {song: "The Box" , lyric: "Bustin' all the bales out the box I just hit a lick with the box" },
+
+    {song: "Highest in the Room" , lyric: "She saw my eyes, she know I'm gone. I see some things that you might fear" },
+    {song: "Highest in the Room" , lyric: "Legs wrapped around my beard Got the fastest car, it zoom" },
+    {song: "Highest in the Room" , lyric: "I see some things that you might fear" },
+    {song: "Highest in the Room" , lyric: "When I'm with you, I feel alive You say you love me, don't you lie" },
+
+    {song: "Circles" , lyric: "Run away, but we're running in circles Run away, run away" },
+    {song: "Circles" , lyric: "I knew that this was doomed from the get go You thought that it was special" },
+    {song: "Circles" , lyric: "I dare you to do something I'm waiting on you again" },
+
+    {song: "Señorita" , lyric: "We danced for hours in the sand Tequila sunrise" },
+    {song: "Señorita" , lyric: "I wish I could pretend I didn't need ya But every touch is ooh la la la" },
+    {song: "Señorita" , lyric: "Land in Miami The air was hot from summer rain" },
+    
+    {song: "I Don't Care" , lyric: "Don't think I fit in at this party Everyone's got so much to say" },
+    {song: "I Don't Care" , lyric: "'Cause I don't care when I'm with my baby, yeah All the bad things disappear" },
+    {song: "I Don't Care" , lyric: "Finish my drink, say, 'Shall we dance?' Hell, yeah" },
+
+    {song: "Bad Guy" , lyric: "White shirt now red, my bloody nose Sleepin', you're on your tippy toes" },
+    {song: "Bad Guy" , lyric: "So you're a tough guy Like it really rough guy" },
+    {song: "Bad Guy" , lyric: "I like it when you take control Even if you know that you don't" },
+
+    {song: "Sunflower" , lyric: "Needless to say, I keep her in check She was a bad bad, nevertheless" },
+    {song: "Sunflower" , lyric: "Crash at my place, baby, you're a wreck Thinkin' in a bad way, losin' your grip" },
+
+    {song: "All I Want for Christmas Is You" , lyric: "Underneath the Christmas tree I don't need to hang my stocking" },
+
+    {song: "Thank U, Next" , lyric: "Thought I'd end up with Sean But he wasn't a match" },
+
+    {song: "I Love It" , lyric: "Your boyfriend is a dork, McLovin (dork, McLovin, ooh, ooh, ooh)" },
+    {song: "I Love It" , lyric: "Look like I fell when I went fishin' (fell) So much diamonds on my bust down" },
+    {song: "I Love It" , lyric: "When the first time they ask you if you want sparklin' or still?" },
+
+    {song: "Lucky You" , lyric: "All my life I want a Grammy but I'll prolly never get it" },
+    {song: "Lucky You" , lyric: "Pass the plate! Where my drink? This my day, lucky you Fuck you too!" },
+
+    {song: "In My Feelings" , lyric: "Say you'll never ever leave from beside me 'Cause I want ya, and I need ya" },
+    {song: "In My Feelings" , lyric: "Look, the new me is really still the real me" },
+
+    {song: "Nonstop" , lyric: "Look, I just flipped the switch I don't know nobody else that's doin' this" },
+
+    {song: "Sad!" , lyric: "I gave her everything She took my heart and left me lonely" },
+    {song: "Sad!" , lyric: "You decide, if you're ever gonna, let me know. Suicide, if you ever try to let go, uh" },
+
+    {song: "This Is America" , lyric: "We just wanna party Party just for you" },
+    {song: "This Is America" , lyric: "Party just for free (yeah) Girl, you got me dancin'" },
+
+    {song: "Better Now" , lyric: "I did not believe that it would end, no Everything came second to the Benzo" },
+    {song: "Better Now" , lyric: "You're not even speaking to my friends, no" },
+    {song: "Better Now" , lyric: "Used to keep my picture posted by your bedside" },
+
+    {song: "Nice for What" , lyric: "Gotta make that jump, gotta make that, gotta, gotta make that" },
+    {song: "Nice for What" , lyric: "Gotta make that jump, gotta make that, gotta, gotta make that" },
+
+    {song: "Call Out My Name" , lyric: "AWe found each other I helped you out of a broken place" },
+    {song: "Call Out My Name" , lyric: "I said I didn't feel nothing baby, but I lied I almost cut a piece of myself" },
+    
+    {song: "God's Plan" , lyric: "Tryna keep it peaceful is a struggle for me Don't pull up at 6 AM to cuddle with me" },
+    {song: "God's Plan" , lyric: "I go hard on Southside G, yuh, Way I make sure that north side eat" },
+    {song: "God's Plan" , lyric: "Yeah they wishin' and wishin' and wishin' and wishin'" },
+    
+    {song: "Havana" , lyric: "Ooh-ooh-ooh, I knew it when I met him I loved him when I left him" },
+    {song: "Havana" , lyric: "And papa says he got malo in him (uh) He got me feelin' like" },
+    
+    {song: "Too Good at Goodbyes" , lyric: "And every time you leave me, the quicker these tears dry" },
+    {song: "Too Good at Goodbyes" , lyric: "I know you're thinking I'm heartless I know you're thinking I'm cold" },
+    
+    {song: "Look What You Made Me Do" , lyric: "I don't like your perfect crime How you laugh when you lie" },
+    {song: "Look What You Made Me Do" , lyric: "Honey, I rose up from the dead, I do it all the time" },
+    
+    {song: "Humble" , lyric: "Ayy, I remember syrup sandwiches and crime allowances" },
+    {song: "Humble" , lyric: "D'usse with my boo bae, tastes like Kool-Aid for the analysts" },
+    {song: "Humble" , lyric: "Pull up on your block, then break it down, we playin' Tetri" },
+    
+    {song: "Shape of You" , lyric: "The club isn't the best place to find a lover So the bar is where I go" },
+    {song: "Shape of You" , lyric: "Your love was handmade for somebody like me Come on now, follow my lead" },
+    
+    {song: "Starboy" , lyric: "I'm tryna put you in the worst mood, P1 cleaner than your church shoes" },
+    {song: "Starboy" , lyric: "House so empty, need a centerpiece Twenty racks a table cut from ebony" },
+    {song: "Starboy" , lyric: "You talkin' 'bout me, I don't see the shade Switch up my style, I take any lane" },
+    
+    {song: "Black Beatles" , lyric: "That girl is a real crowd pleaser Small world, all her friends know me" },
+    {song: "Black Beatles" , lyric: "Release the cash, watch it fall slowly Frat girls still tryna get even" },
+    
+    {song: "Closer" , lyric: "And four years, no calls Now you're looking pretty in a hotel bar" },
+    {song: "Closer" , lyric: "So baby, pull me closer in the backseat of your Rover That I know you can't afford" },
+    
+    {song: "Cold Water" , lyric: "" },
+    {song: "Cold Water" , lyric: "" },
+    
+    {song: "Love Yourself" , lyric: "" },
+    {song: "Love Yourself" , lyric: "" },
+    
+    {song: "Sorry" , lyric: "" },
+    {song: "Sorry" , lyric: "" },
+    
+    {song: "Hello" , lyric: "" },
+    {song: "Hello" , lyric: "" },
+    
+    {song: "What Do You Mean?" , lyric: "" },
+    {song: "What Do You Mean?" , lyric: "" },
+    
+    {song: "Can't Feel My Face" , lyric: "" },
+    {song: "Can't Feel My Face" , lyric: "" },
+    
+    {song: "Drag Me Down" , lyric: "" },
+    {song: "Drag Me Down" , lyric: "" },
+    {song: "Drag Me Down" , lyric: "" },
+    
+    {song: "Lean On" , lyric: "" },
+    {song: "Lean On" , lyric: "" },
+    
+    {song: "See You Again" , lyric: "" },
+    {song: "See You Again" , lyric: "" },
+    {song: "See You Again" , lyric: "" },
+    
+    {song: "FourFiveSeconds" , lyric: "" },
+    {song: "FourFiveSeconds" , lyric: "" },
+    
+    {song: "Uptown Funk" , lyric: "" },
+    {song: "Uptown Funk" , lyric: "" },
+    {song: "Uptown Funk" , lyric: "" },
+    
+    {song: "Take Me to Church" , lyric: "" },
+    {song: "Take Me to Church" , lyric: "" },
+    {song: "Take Me to Church" , lyric: "" },
+    
+    {song: "Thinking Out Loud" , lyric: "" },
+    {song: "Thinking Out Loud" , lyric: "" },
+    
+    {song: "Shake It Off" , lyric: "" },
+    {song: "Shake It Off" , lyric: "" },
+    
+    {song: "Blame" , lyric: "" },
+    {song: "Blame" , lyric: "" },
+    
+    {song: "Rude" , lyric: "" },
+    {song: "Rude" , lyric: "" },
+    
+    {song: "Summer" , lyric: "" },
+    
+    {song: "Rather Be" , lyric: "" },
+    {song: "Rather Be" , lyric: "" },
+    
+    {song: "Dark Horse" , lyric: "" },
+    
+    {song: "Timber" , lyric: "" },
+    
+    {song: "The Monster" , lyric: "" },
+    {song: "The Monster" , lyric: "" },
+    
+    {song: "Royals" , lyric: "" },
+    
+    {song: "Wrecking Ball" , lyric: "" },
+    {song: "Wrecking Ball" , lyric: "" },
+    
+    {song: "Pillowtalk" , lyric: "" },
+    {song: "Pillowtalk" , lyric: "" },
+    {song: "Pillowtalk" , lyric: "" },
+    
+    {song: "Alright" , lyric: "" },
+    {song: "Alright" , lyric: "" },
+    {song: "Alright" , lyric: "" },
+
+    {song: "Gooba" , lyric: "" },
+    {song: "Gooba" , lyric: "" },
+]
