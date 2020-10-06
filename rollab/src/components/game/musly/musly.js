c3b2846a2ec1e130200bc3fb1bfb7de8b3ed8ac5
Aajinkya Singh <43881544+aajinkya1203@users.noreply.github.com>
2020-10-06 10:06:10 +0530

    Added lyrics

--- rollab/src/components/game/musly/musly.js
+++ rollab/src/components/game/musly/musly.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react'
 import { getLyrics } from 'genius-lyrics-api';
 import { apiKey } from '../../keys/key';
+import { split } from 'lodash';
 
 const Musly = () => {
     useEffect(()=>{
         const options = {
             apiKey,
-            title: 'Blinding Lights',
+            title: 'blinding lights',
             artist: '',
             optimizeQuery: true
         };
         getLyrics(options).then((lyrics)=>{
             console.log(lyrics)
+            console.log(lyrics.length)
+            lyrics = split(lyrics, "\n");
+            
         })
     }, [])
     return (
